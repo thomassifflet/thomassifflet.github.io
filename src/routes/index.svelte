5c3c983224ac34c42a6c7a79cc76b123d6040f7a
Thomas S. <thomas.sifflet@gmail.com>
2023-10-26 16:14:41 +0200

    template

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<!-- /src/routes/index.svelte -->
+<style>
+    main {
+        padding: 4vw;
+    }
+   </style>
+   
+   <main>
+       <h1>Posts</h1>
+       <p>Here we will place all the different blog posts</p>
+   </main>
